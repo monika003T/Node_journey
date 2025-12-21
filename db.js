b1b6709fe5a03a9912d0022dd6ee17ae85b3c1c8
monika003T <monikathakur2062@gmail.com>
2025-12-21 15:39:42 +0530

    mongodb node connection

---
+++ db.js
@@ -0,0 +1,89 @@
+// Mongodb is a nosql database
+const express= require("express");
+const users=require("./MOCK_DATA.json")
+const fs=require("fs")
+
+const mongoose = require("mongoose");
+const { timeStamp } = require("console");
+
+const app=express();
+const PORT=8000;
+
+app.use(express.urlencoded({extended:false}))
+
+/* how to connect mongodb to nodejs
+ 1. make schema- schema is a structure for a document
+2. make model for that schema 
+3.connect to database
+4. using that model perform crud operation */
+
+
+// schema
+const userSchema= new mongoose.Schema({
+first_Name:{
+    type:String,
+    required:true,
+},
+last_Name:{
+    type:String,
+    required:true,
+},
+email:{
+    type:String,
+    required:true,
+},
+gender:{
+    type:String,
+    
+},
+job_title:{
+    type:String,
+}
+},{timeStamp:true});
+
+// model
+const User=mongoose.model('user',userSchema);
+
+// connection
+mongoose
+.connect('mongodb://127.0.0.1:27017/learningdb')
+.then(()=>console.log('mongodb connected'))
+.catch((err)=>console.log('Mongo err',err));
+
+
+app.get("/users", async (req, res)=>{
+    const allusers=await User.find({});
+    const html=`
+    <ul>
+        ${allusers.map((user)=>`<li>${user.first_Name}</li>`).join("")}
+        </ul>`;
+        res.send(html)
+})
+app.post("/api/users",async (req,res)=>{
+    const body=req.body;
+    if(!body||
+        !body.first_name||
+        !body.last_name||
+        !body.email||
+        !body.gender||
+        !body.job_title){
+        //400 bad request
+        return res.status(400).json({msg:"all fields required"})
+    }
+     const result= await User.create({
+        first_Name:body.first_name,
+        last_Name:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        job_title:body.job_title,
+
+    })
+    console.log("result",result)
+    return res.status(201).json({msg:"success"})
+    
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server Started at PORT ${PORT}`)
+})
